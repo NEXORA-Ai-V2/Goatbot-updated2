82fc523a4d95b42544c7057242fc822b4f349490
NEXORA-Ai-V2 <misssenorita38@gmail.com>
2026-08-23 17:55:30 +0600

    Refactor rip command for simplicity and efficiency

--- scripts/cmds/rip.js
+++ scripts/cmds/rip.js
@@ -1,12 +1,12 @@
+
 const DIG = require("discord-image-generation");
 const fs = require("fs-extra");
-const path = require("path");
 
 module.exports = {
   config: {
     name: "rip",
-    version: "1.1.1",
-    author: "dipto | opu",
+    version: "1.1",
+    author: "Toshiro Editz",
     countDown: 5,
     role: 0,
     shortDescription: "rip image",
@@ -14,45 +14,19 @@ module.exports = {
     category: "fun",
     guide: {
       vi: "{pn} [@tag | blank]",
-      en: "{pn} [@tag | reply]"
+      en: "{pn} [@tag]"
     }
   },
 
   onStart: async function ({ event, message, usersData }) {
-    let uid;
-    const mentions = Object.keys(event.mentions || {});
-
-    if (mentions.length > 0) {
-      uid = mentions[0];
-    } else if (event.type === "message_reply" || event.messageReply) {
-      uid = event.messageReply.senderID;
-    } else {
-      return message.reply("Please mention someone or reply to a message!");
-    }
-
-    try {
-      const avatarURL = await usersData.getAvatarUrl(uid);
-      const img = await new DIG.Rip().getImage(avatarURL);
-
-      const tmpDir = path.join(__dirname, "tmp");
-      fs.ensureDirSync(tmpDir);
-
-      const pathSave = path.join(tmpDir, `${uid}_Rip.png`);
-      fs.writeFileSync(pathSave, Buffer.from(img));
-
-      return message.reply(
-        {
-          attachment: fs.createReadStream(pathSave)
-        },
-        () => {
-          if (fs.existsSync(pathSave)) {
-            fs.unlinkSync(pathSave);
-          }
-        }
-      );
-    } catch (error) {
-      console.error("RIP command error:", error);
-      return message.reply("An error occurred while generating the image. Please try again!");
-    }
+ const uid = Object.keys(event.mentions)[0]
+ if(!uid) return message.reply("please mention someone")
+    const avatarURL = await usersData.getAvatarUrl(uid);
+    const img = await new DIG.Rip().getImage(avatarURL);
+ const pathSave = `${__dirname}/tmp/${uid}_Rip.png`;
+    fs.writeFileSync(pathSave, Buffer.from(img));
+    message.reply({
+      attachment: fs.createReadStream(pathSave)
+    }, () => fs.unlinkSync(pathSave));
   }
 };
